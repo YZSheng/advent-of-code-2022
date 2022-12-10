8636b00213b9a2cbc3a79fedc73620a0ea0521c8
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-12-10 10:35:17 +0800

    Add solution to part 2 day 1

--- src/aoc2022/day01/solution.clj
+++ src/aoc2022/day01/solution.clj
@@ -10,4 +10,18 @@
        (map #(reduce + %))
        (apply max)))
 
+(defn solve-part-two [input]
+  (->> input
+       slurp
+       (#(string/split % #"\n\n"))
+       (map string/split-lines)
+       (map (fn [nums] (map #(Integer/parseInt %) nums)))
+       (map #(reduce + %))
+       (sort >)
+       (take 3)
+       (#(reduce + %))))
+
 (solve-part-one "resources/day01/part-one.txt")
+
+(solve-part-two "resources/day01/sample.txt")
+(solve-part-two "resources/day01/part-one.txt")
